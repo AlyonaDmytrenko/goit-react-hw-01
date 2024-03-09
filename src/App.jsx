7b59afe4986347eb47f6b15275c17a71cb70dda9
AlyonaDmytrenko <143014363+AlyonaDmytrenko@users.noreply.github.com>
2024-03-09 15:25:50 +0200

    create profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import Profile from './components/Profile/Profile';
-import './App.css';
 
-function App() {
+import userData from './userData.json';
+
+const App = () => {
   return (
-    <div>
-      <Profile />
-    </div>
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
   );
-}
+};
 
 export default App;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,36 @@
-const Profile = () => {
+const Profile = ({
+  name,
+  tag,
+  location,
+  image,
+  followers,
+  views,
+  likes,
+  stats,
+}) => {
   return (
     <div>
-      <h2>Profile</h2>
+      <div>
+        <img src={image} alt="User avatar" />
+        <p>{name}</p>
+        <p>{tag}</p>
+        <p>{location}</p>
+      </div>
+
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>{stats.followers}</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>{stats.views}</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>{stats.likes}</span>
+        </li>
+      </ul>
     </div>
   );
 };
